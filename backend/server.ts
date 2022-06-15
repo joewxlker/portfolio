a7477faf59995cf40f57fda5e738544f3c626bdd
joseph <joewxlk3r@gmail.com>
2022-06-15 19:02:46 +0800

    first commit

--- backend/server.ts
+++ backend/server.ts
@@ -1,28 +1,15 @@
-// wss://autumn-sparkling-resonance.bsc.quiknode.pro/2519c42cdc27be9ccf32638d0878ed819cd3cff7/ binance
-// wss://cool-wispy-feather.ropsten.quiknode.pro/e9f0b6b76ce4bdfedb224edd8279642111a45317/ ropsten
-// wss://purple-red-paper.bsc.quiknode.pro/ac051feae4b6bac81afa8edce0e30f0ab2f6e669/ binance 2
 const express = require("express");
 const http = require('http');
 const Web3 =  require("web3")
-const ReactDOMServer = require('react-dom/server');
-const ethers = require("ethers");
 const app = express(); 
 const PORT = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
-const router = express.Router()
-
-// to interact with the blockchain using this source code you will need a node end point using the same network you wish to use 
-
 var wss = "wss://cool-wispy-feather.ropsten.quiknode.pro/e9f0b6b76ce4bdfedb224edd8279642111a45317/"; 
 const web3 = new Web3(wss);
 const abi = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"string","name":"_username","type":"string"},{"indexed":true,"internalType":"uint256","name":"UserID","type":"uint256"},{"indexed":true,"internalType":"address","name":"UserAddress","type":"address"}],"name":"AccountCreated","type":"event"},{"inputs":[{"internalType":"address","name":"_receiver","type":"address"},{"internalType":"address","name":"_sender","type":"address"}],"name":"addFriend","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_usernames","type":"string"},{"internalType":"address","name":"newUser","type":"address"}],"name":"createAccount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_sender","type":"address"}],"name":"delChatActive","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"getAccountInfo","outputs":[{"internalType":"string","name":"","type":"string"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_sender","type":"address"}],"name":"getActiveChat","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getAllUsers","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"friend","type":"address"},{"internalType":"address","name":"friend2","type":"address"}],"name":"getChatCode","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"_sender","type":"address"}],"name":"getFriends","outputs":[{"components":[{"internalType":"address","name":"friend","type":"address"}],"internalType":"struct Messenger.friends[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"friend_key","type":"bytes32"}],"name":"readMessage","outputs":[{"components":[{"internalType":"address","name":"reciever","type":"address"},{"internalType":"string","name":"_message","type":"string"}],"internalType":"struct Messenger.message[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_sender","type":"address"},{"internalType":"address","name":"_receiver","type":"address"},{"internalType":"string","name":"_msg","type":"string"}],"name":"sendMessage","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_sender","type":"address"},{"internalType":"address","name":"_receiver","type":"address"}],"name":"setChatActive","outputs":[],"stateMutability":"nonpayable","type":"function"}]
 var contractAddress = "0x145b48cF16EF3A150322D0da0452f73FE4A50492"; 
 var myContract = new web3.eth.Contract(abi, contractAddress);
 const networkId = web3.eth.net.getId();
-
-//for node to call function and send wei to the block chain it will require you to use your own wallet and wei to handle these transactions
-// multiple wallets allows for synchronous transactions without altering gas prices
-
 const secretKey = "8b299edbf9bffbc1833facc6a598d685ff485911e167d9bcb6e769e6db5a02c8" 
 const secretKeyTwo = "697fdb9b635d6e4175b6041bea825031fd695bebb4f38ee2d30233230d278783"
 const address = "0x7831E194F6DF6f55f1aE56e6EDcbEE64E9e9F720"
@@ -32,10 +19,7 @@ const addressTwo = "0x04b4dbA9D2F34F007F55e350BCFf0DB318003a68"
 //------------------------------------------------------------------------->Create Server<-------------------------------------------------------------------------------------------------
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use('/', router);  
-//now we create the express server
 const server = http.createServer(app); 
-// we launch the server
 server.listen(PORT, () => { 
     console.log(`Listening on port ${PORT}`)
 });
