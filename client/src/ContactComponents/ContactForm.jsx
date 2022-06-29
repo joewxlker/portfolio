4455feffcaff2a59dae620968f156322a91d4d88
joseph <joewxlk3r@gmail.com>
2022-06-29 14:42:57 +0800

    ui fixes

--- client/src/ContactComponents/ContactForm.jsx
+++ client/src/ContactComponents/ContactForm.jsx
@@ -41,29 +41,33 @@ const ContactForm = () => {
     return (
         <>
             <div className='overlayContainer'>
-            <div className='Contact-Form-Overlay bg-dark' />
+                <div className='Contact-Form-Overlay bg-dark' />
             </div>
-            <div className='Contact-Form-Main d-flex flex-column align-items-center justify-content-center bg-dark'>
-                <h1 className='text-light'>Contact Me</h1>
-                <div className='Form-Container text-accent d-flex flex-column justify-content-center align-items-center'>
-                    <form className='w-100 d-flex flex-row justify-content-center align-items-center flex-wrap' onSubmit={handleSubmit}>
-                        <div className='m-3 d-flex flex-column justify-content-center'>
-                            name:
-                            <input className='' type='text' title='name input' name='name' value={value.name} onChange={setName}>
-                            </input>
-                        </div>
-                        <div className='d-flex flex-column justify-content-center'>
-                            email:
-                            <input className='' type='text' title='email input' name='email' value={value.email} onChange={setEmail}>
-                            </input>
-                        </div>
-                        <div className='m-3 d-flex flex-column justify-content-center'>
-                        message:
-                            <input className='' type='text' title='message input' name='message' value={value.message} onChange={setMessage}>
-                            </input>
-                        </div>
-                        <button className='bg-accent btn' type='submit'>send</button>
-                    </form>
+            <div className='w-100 d-flex flex-row'>
+                <div className='Contact-Form-Main d-flex flex-column align-items-center justify-content-center bg-dark'>
+                    <h1 className='text-light'>Contact Me</h1>
+                    <div className='Form-Container text-accent d-flex flex-column justify-content-center align-items-center'>
+                        <form className='w-75 d-flex flex-row justify-content-center align-items-center flex-wrap' onSubmit={handleSubmit}>
+                            <div className=' d-flex flex-column justify-content-center'>
+                                name:
+                                <input className='' type='text' title='name input' name='name' value={value.name} onChange={setName}>
+                                </input>
+                            </div>
+                            <div className='d-flex flex-column justify-content-center'>
+                                email:
+                                <input className='' type='text' title='email input' name='email' value={value.email} onChange={setEmail}>
+                                </input>
+                            </div>
+                            <div className='m-3 d-flex flex-column justify-content-center'>
+                                message:
+                                <input className='' type='text' title='message input' name='message' value={value.message} onChange={setMessage}>
+                                </input>
+                            </div>
+                            <button className='bg-accent btn w-100' type='submit'>send</button>
+                        </form>
+                    </div>
+                </div>
+                <div className='contact-form-right w-50 bg-dark'>
                 </div>
             </div>
         </>
