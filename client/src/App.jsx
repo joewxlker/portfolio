f38f650eba8adcae6e1407b6ddef673373eb59c1
joseph <joewxlk3r@gmail.com>
2022-06-29 16:17:00 +0800

    debugging production blank screenonly rendering header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 
 import React from 'react'
-import Main from './MainEntryComponents/Main/Main.jsx'
+// import Main from './MainEntryComponents/Main/Main.jsx'
 import { Fragment } from 'react';
 import Header from './MainEntryComponents/Header/Header.jsx'
-import PolygonOverlay from './MainEntryComponents/Overlays/PolygonOverlay'
+// import PolygonOverlay from './MainEntryComponents/Overlays/PolygonOverlay'
 // import Projects from './ProjectComponents/Projects'
 // import ContactForm from './ContactComponents/ContactForm';
 // import SolutionsMain from './SolutionsComponents/SolutionsMain';
@@ -14,8 +14,8 @@ const App = () => {
     return (
       <Fragment>
         <Header />
-        <Main />
-        <PolygonOverlay />
+        {/* <Main /> */}
+        {/* <PolygonOverlay /> */}
         {/* <Projects /> */}
         {/* <SolutionsMain /> */}
         {/* <ContactForm /> */}
