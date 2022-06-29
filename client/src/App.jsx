c598516bf61513b85959b158dec37d3c2d9870f0
joseph <joewxlk3r@gmail.com>
2022-06-29 16:33:54 +0800

    last attempt didnt work, going back to no components and a single h1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,25 @@
 
 import React from 'react'
-import Main from './MainEntryComponents/Main.jsx'
-import { Fragment } from 'react';
-import Header from './Header/Header.jsx'
-import PolygonOverlay from './Overlays/PolygonOverlay'
-import Projects from './ProjectComponents/Projects'
-import ContactForm from './ContactComponents/ContactForm';
-import SolutionsMain from './SolutionsComponents/SolutionsMain';
+// import Main from './MainEntryComponents/Main.jsx'
+// import Header from './Header/Header.jsx'
+// import PolygonOverlay from './Overlays/PolygonOverlay'
+// import Projects from './ProjectComponents/Projects'
+// import ContactForm from './ContactComponents/ContactForm';
+// import SolutionsMain from './SolutionsComponents/SolutionsMain';
 
 
 const App = () => {
   
     return (
-      <Fragment>
+      <>
         <h1>hello</h1>
-        <Header />
+        {/* <Header />
         <Main />
         <PolygonOverlay />
         <Projects />
         <SolutionsMain />
-        <ContactForm />
-      </Fragment>
+        <ContactForm /> */}
+      </>
     );
   };
 
